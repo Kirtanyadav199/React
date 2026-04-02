ce1bf5e1ecd79af8e8f30a635d4372f6abb951e6
Kirtanyadav199 <kirtanyadav24@gmail.com>
2026-04-02 11:51:59 +0530

    React

--- class5/src/App.jsx
+++ class5/src/App.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React, { use } from 'react'
 import { useState } from 'react'
 
 const App = () => {
-  
-  const[king,setKing] = useState("kirtan")
+  const [marks,setMarks] = useState([50,78,90,12,25])
+   
 
-  function changeking(){
-    console.log("button clicked")
-    setKing("Amrish")
+  function giveGrace(){
+    const newMarks = marks.map(function(elem){
+      return elem+5;
+    })
+    setMarks(newMarks)
   }
+
   return (
-    <div>
-      <h1>{king}</h1>
-      <button onClick={changeking}>changeking</button>
+    <div className=''>
+     {marks.map(function(elem,idx){
+      return <h1 key={idx} >Student {idx+1} = {elem} ({elem>33?'pass':'fail'})</h1>
+     })}
+     <button onClick={giveGrace}>Give them grace</button>
+    
     </div>
   )
 }
