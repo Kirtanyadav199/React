1b57b8aa1cb6fdfd14c880896af69d85ceb4c3aa
Kirtanyadav199 <kirtanyadav24@gmail.com>
2026-04-03 07:05:02 +0530

    React

--- class-6/src/App.jsx
+++ class-6/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const App = () => {
+
+  function submitHandler(e){
+   e.preventDefault(e);
+    console.log("submitted")
+  }
   return (
     <div>
-      
+      <form onSubmit={(e)=>{
+        submitHandler(e);
+      }}>
+        <input type="text" placeholder='Enter Name' />
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
